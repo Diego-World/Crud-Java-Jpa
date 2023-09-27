1d5b4b4ce64531edc501bd55bacce46500d4f6d3
Diego-World <contato.diegogouveia@gmail.com>
2023-09-26 23:34:41 -0300

    Watch interface

---
+++ src/main/java/com/gouveia/crud/crudjpa/repository/WatchRepository.java
@@ -0,0 +1,9 @@
+package com.gouveia.crud.crudjpa.repository;
+
+import com.gouveia.crud.crudjpa.model.Watch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WatchRepository extends JpaRepository<Long, Watch> {
+}
