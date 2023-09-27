b4c03575c67007370e1921af592a61859f35ae7c
Diego-World <contato.diegogouveia@gmail.com>
2023-09-26 23:52:42 -0300

    Create service created, from now i will work on main branch

--- src/main/java/com/gouveia/crud/crudjpa/service/WatchService.java
+++ src/main/java/com/gouveia/crud/crudjpa/service/WatchService.java
@@ -1,5 +1,6 @@
 package com.gouveia.crud.crudjpa.service;
 
+import com.gouveia.crud.crudjpa.model.Watch;
 import com.gouveia.crud.crudjpa.repository.WatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,14 @@ public class WatchService {
     @Autowired
     WatchRepository watchRepository;
 
-
+    //CREATE, READ, UPDATE AND DELETE
+    String postWatch(Watch watch){
+        Watch w = new Watch();
+        w.setBrand(watch.getBrand());
+        w.setPrice(watch.getPrice());
+        w.setProductName(watch.getProductName());
+        watchRepository.save(w.getId());
+        return "Product save sucessfuly";
+    }
 
 }
